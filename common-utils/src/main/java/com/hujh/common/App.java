73530c32cbbb54bbe5390e205df115f927000466
hujha <hujha@chanjet.com>
2017-03-08 15:26:06 +0800

    2

---
+++ common-utils/src/main/java/com/hujh/common/App.java
@@ -0,0 +1,13 @@
+package com.hujh.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
